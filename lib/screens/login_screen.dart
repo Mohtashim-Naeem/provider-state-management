08cb9274c7dddfcd804cf399b4c91bacb1778f23
mohtashim <26.april.motu@gmail.com>
2024-02-19 13:25:59 +0500

    bus feedback app all code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -290,14 +290,14 @@ class _LoginScreenState extends State<LoginScreen> {
                   //   decoration: BoxDecoration(
                   //       borderRadius: BorderRadius.circular(50),
                   //       gradient: LinearGradient(colors: [
-                  //         // Color(0xff1D2671),
-                  //         // Color(0xffC33764),
+                          // Color(0xff1D2671),
+                          // Color(0xffC33764),
 
-                  //         // Color.fromARGB(255, 41, 63, 132),
-                  //         Color.fromARGB(255, 189, 138, 49),
+                          // Color.fromARGB(255, 41, 63, 132),
+                          // Color.fromARGB(255, 189, 138, 49),
 
-                  //         // Color.fromARGB(255, 56, 154, 71),
-                  //         // Color.fromARGB(255, 56, 154, 71),
+                          // Color.fromARGB(255, 56, 154, 71),
+                          // Color.fromARGB(255, 56, 154, 71),
                   //         Colors.white,
 
                   //         // Color.fromARGB(255, 68, 158, 74),
